c3b783df4c5580015b0b0e871b5f2367233c28de
Ace Dev Fun Games <rodrigoacevedo@outlook.com>
2018-07-12 20:09:41 -0300

    Ajustes no Endpoint /categorias/{id}

---
+++ src/main/java/com/acedevfum/cursomc/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,17 @@
+package com.acedevfum.cursomc.services.exceptions;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	public ObjectNotFoundException(String msg) {
+		super(msg) ;
+	}
+
+	
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+		
+	}
+}
