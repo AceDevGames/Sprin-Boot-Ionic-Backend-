f0ee5e4fffcef9dbb5012334fa332d1c1eff58d0
Ace Dev Fun Games <rodrigoacevedo@outlook.com>
2018-07-16 15:37:56 -0300

    Listando todas categorias com DTO

--- src/main/java/com/acedevfum/cursomc/services/CategoriaService.java
+++ src/main/java/com/acedevfum/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.acedevfum.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +46,10 @@ public class CategoriaService {
 		
 	}
 	
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
 	}
 
 
