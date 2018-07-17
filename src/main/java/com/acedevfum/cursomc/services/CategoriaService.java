63e11ab979283133a1d5999f1dea4f41230b8394
Ace Dev Fun Games <rodrigoacevedo@outlook.com>
2018-07-16 22:42:57 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/acedevfum/cursomc/services/CategoriaService.java
+++ src/main/java/com/acedevfum/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.acedevfum.cursomc.domain.Categoria;
+import com.acedevfum.cursomc.dto.CategoriaDTO;
 import com.acedevfum.cursomc.repositories.CategoriaRepository;
 import com.acedevfum.cursomc.services.exceptions.DataIntegrityException;
 import com.acedevfum.cursomc.services.exceptions.ObjectNotFoundException;
@@ -56,6 +57,11 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
 
